676a6fcc0daef57a7b729bf06d87632e5456e1af
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-07-12 11:01:51 +0700

    create builder test for bike

--- creational-patterns/builder/f/builder_test.go
+++ creational-patterns/builder/f/builder_test.go
@@ -7,15 +7,33 @@ import (
 )
 
 func TestBuilderPattern(t *testing.T) {
-	manufacturingComplex := &ManufacturingDirector{}
 
-	carBuilder := &CarBuilder{}
-	manufacturingComplex.SetBuilder(carBuilder)
-	manufacturingComplex.Construct()
+	t.Run("car builder", func(t *testing.T) {
+		manufacturingComplex := &ManufacturingDirector{}
 
-	car := carBuilder.GetVehicle()
+		carBuilder := &CarBuilder{}
+		manufacturingComplex.SetBuilder(carBuilder)
+		manufacturingComplex.Construct()
 
-	assert.Equal(t, car.Wheels, 4)
-	assert.Equal(t, car.Structure, "Car")
-	assert.Equal(t, car.Seats, 5)
+		car := carBuilder.GetVehicle()
+
+		assert.Equal(t, car.Wheels, 4)
+		assert.Equal(t, car.Structure, "Car")
+		assert.Equal(t, car.Seats, 5)
+	})
+
+	t.Run("bike builder", func(t *testing.T) {
+		manufacturingComplex := &ManufacturingDirector{}
+
+		bikeBuilder := &BikeBuilder{}
+		manufacturingComplex.SetBuilder(bikeBuilder)
+		manufacturingComplex.Construct()
+
+		motorbike := bikeBuilder.GetVehicle()
+		motorbike.Seats = 1
+
+		assert.Equal(t, motorbike.Wheels, 2)
+		assert.Equal(t, motorbike.Structure, "Motorbike")
+	})
+
 }
